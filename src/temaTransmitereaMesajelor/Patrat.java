7b278fa108268213c28e6905bc1beb986e520207
eric <ericpredit@gmail.com>
2025-03-22 15:07:03 +0200

    Patrat class & 2 constructors created

---
+++ src/temaTransmitereaMesajelor/Patrat.java
@@ -0,0 +1,20 @@
+package temaTransmitereaMesajelor;
+
+public class Patrat {
+	
+	int latura;
+	
+	public Patrat() {
+		latura = 10;
+	}
+	
+	public Patrat(int latura) {
+		this.latura = latura;
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}

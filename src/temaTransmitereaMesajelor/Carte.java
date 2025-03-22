ef65487b6910c0bc9731a44e7662afecd5bc6f87
eric <ericpredit@gmail.com>
2025-03-22 15:02:21 +0200

    method comparing 2 objects created

--- src/temaTransmitereaMesajelor/Carte.java
+++ src/temaTransmitereaMesajelor/Carte.java
@@ -10,19 +10,24 @@ public class Carte {
 	
 	public void potrivita(Carte carte1, Carte carte2) {
 		if(carte1.nrPagini == carte2.nrPagini)
-			System.out.println("da");
+			System.out.println("carte1 si carte2 au acelasi numar de pagini");
+		else if(carte1.nrPagini > carte2.nrPagini)
+			System.out.println("carte1 are mai multe pagini");
+		else
+			System.out.println("carte2 are mai multe pagini");
 	}
 	
-	public String toString() {
-		return "hello world " + nrPagini;
-	}
+//	public String toString() {
+//		return "hello world " + nrPagini;
+//	}
 
 	public static void main(String[] args) {
 		
 		Carte carte1 = new Carte(200);
-		Carte carte2 = new Carte(200);
+		Carte carte2 = new Carte(100);
 
 		carte1.potrivita(carte1, carte2);
+		
 	}
 
 }

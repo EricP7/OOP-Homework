ade8007890441c2da480a5adbc60ba89597e0ecc
eric <ericpredit@gmail.com>
2025-03-22 15:18:23 +0200

    toString() method, objects created

--- src/temaTransmitereaMesajelor/Patrat.java
+++ src/temaTransmitereaMesajelor/Patrat.java
@@ -11,9 +11,29 @@ public class Patrat {
 	public Patrat(int latura) {
 		this.latura = latura;
 	}
+	
+	/*nu am inteles daca trebuie sa facem o metoda
+	 * care se numeste 'potrivita' 
+	 * sau sa facem o metoda potrivita pentru o anumita actiune*/
+	public void potrivita() {
+		
+	}
+	
+	public String toString() {
+		return "Patrat " + latura + " aria " + latura*latura;
+	}
 
 	public static void main(String[] args) {
 		
+		Patrat patrat1 = new Patrat();
+		Patrat patrat2 = new Patrat(8);
+		Patrat patrat3 = new Patrat(12);
+		Patrat patrat4 = new Patrat();
+		
+		System.out.println(patrat1);
+		System.out.println(patrat2);
+		System.out.println(patrat3);
+		System.out.println(patrat4);
 
 	}
 

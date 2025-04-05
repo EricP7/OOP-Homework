4fd0b2ea67f8744fbf7356bca790a6e6c21fd42b
eric <ericpredit@gmail.com>
2025-04-05 23:23:35 +0300

    refuel()

--- src/relatieMostenire/TomCat.java
+++ src/relatieMostenire/TomCat.java
@@ -1,4 +1,8 @@
 package relatieMostenire;
 
 public class TomCat extends AvionLupta{
+
+    public void refuel(){
+        System.out.println(getPlaneID() + " - Initiating refueling procedure - Locating refueller - Catching up - Refueling - Refueling complete");
+    }
 }

ab3a7bbc944f0a21730ed88a412a204cf04ff76a
eric <ericpredit@gmail.com>
2025-04-05 22:58:41 +0300

    planeID & totalEnginePower + getters

---
+++ src/relatieMostenire/Avion.java
@@ -0,0 +1,14 @@
+package relatieMostenire;
+
+public class Avion {
+
+    private String planeID;
+    private int totalEnginePower;
+
+    public String getPlaneID(){
+        return planeID;
+    }
+    public int getTotalEnginePower(){
+        return totalEnginePower;
+    }
+}

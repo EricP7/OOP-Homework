746c0ec9bc5cd6a4e63ec9267009bf10001625b6
eric <ericpredit@gmail.com>
2025-04-05 23:22:09 +0300

    highSpeed & normal geometry methods

--- src/relatieMostenire/Mig.java
+++ src/relatieMostenire/Mig.java
@@ -1,4 +1,11 @@
 package relatieMostenire;
 
 public class Mig extends AvionLupta{
+
+    public void highSpeedGeometry(){
+        System.out.println(getPlaneID() + " - High speed geometry selected");
+    }
+    public void normalGeometry(){
+        System.out.println(getPlaneID() + " - Normal geometry selected");
+    }
 }

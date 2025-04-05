ba9bf5b000f3f18d626c50cd338d847787b3c0e9
eric <ericpredit@gmail.com>
2025-04-05 23:12:41 +0300

    takeoff(), land() & fly()

--- src/relatieMostenire/Avion.java
+++ src/relatieMostenire/Avion.java
@@ -11,4 +11,14 @@ public class Avion {
     public int getTotalEnginePower(){
         return totalEnginePower;
     }
+
+    public void takeOff(){
+        System.out.println(planeID + " - Initiating takeoff procedure - Starting engines - Accelerating down the runway - Taking o - Retracting gear - Takeoff complete");
+    }
+    public void land(){
+        System.out.println(planeID + "Initiating landing procedure - Enabling airbrakes - Lowering gear - Contacting runway - Decelerating - Stopping engines - Landing complete");
+    }
+    public void fly(){
+        System.out.println(planeID + "Flying");
+    }
 }

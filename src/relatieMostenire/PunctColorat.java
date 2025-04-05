92b30385f5d61fd60b7810426fc5201e70088e85
eric <ericpredit@gmail.com>
2025-04-05 22:32:34 +0300

    clasa extinsa PunctColorat

---
+++ src/relatieMostenire/PunctColorat.java
@@ -0,0 +1,10 @@
+package relatieMostenire;
+
+public class PunctColorat extends Punct{
+
+    private String c;
+    public PunctColorat(double x, double y, String c) {
+        super(x, y);
+        this.c = c;
+    }
+}

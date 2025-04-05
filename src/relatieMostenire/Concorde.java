1abe3526e144b93ecabcca058cfd84d1a67dc715
eric <ericpredit@gmail.com>
2025-04-05 23:17:41 +0300

    supersonic & subsonic methods

--- src/relatieMostenire/Concorde.java
+++ src/relatieMostenire/Concorde.java
@@ -1,4 +1,11 @@
 package relatieMostenire;
 
 public class Concorde extends AvionCalatori{
+
+    public void goSuperSonic(){
+        System.out.println(getPlaneID() + " - Supersonic mode activated");
+    }
+    public void goSubSonic(){
+        System.out.println(getPlaneID() + " - Supersonic mode deactivated");
+    }
 }

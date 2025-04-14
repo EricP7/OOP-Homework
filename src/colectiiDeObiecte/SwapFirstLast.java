4afc68977c75579db30d78b57beff6972aa33edd
eric <ericpredit@gmail.com>
2025-04-14 19:18:30 +0300

    swapFirstLast() method

---
+++ src/colectiiDeObiecte/SwapFirstLast.java
@@ -0,0 +1,37 @@
+package colectiiDeObiecte;
+
+import java.util.ArrayList;
+
+public class SwapFirstLast {
+
+    public ArrayList<String> swapFirstLast(ArrayList<String> list){
+        String aux;
+        if(list.size() > 1){
+            aux = list.getFirst();
+            list.set(0, list.getLast());
+            list.set(list.size() - 1, aux);
+
+            return list;
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+
+        SwapFirstLast swap = new SwapFirstLast();
+
+        ArrayList<String> list1 = new ArrayList<String>();
+        list1.add("hello");
+        list1.add("world");
+
+        ArrayList<String> list2 = new ArrayList<String>();
+        list2.add("hello");
+
+        ArrayList<String> list3 = new ArrayList<String>();
+
+        System.out.println(swap.swapFirstLast(list1));
+        System.out.println(swap.swapFirstLast(list2));
+        System.out.println(swap.swapFirstLast(list3));
+
+    }
+}
